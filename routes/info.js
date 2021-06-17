84cc952bef9e432680f115be60aae2f8081d2dea
Minh Tài <tranminhtai365@gmail.com>
2021-06-17 11:39:56 +0700

    edit info

--- routes/info.js
+++ routes/info.js
@@ -11,64 +11,66 @@ router.get('/', (req, res, next) => {
   })
 })
 router.post('/set', async (req, res, next) => {
-  const { _id, name, email, address, phone, facebook, zalo, tiktok, gps, introduce, paypolicy, shippolicy, warrantypolicy, logo } = req.body;
+  const { _id, name, email, address, phone, facebook, zalo, tiktok, youtube, introduce, paypolicy, shippolicy, warrantypolicy, logo } = req.body;
   try {
     const fileStr = logo;
     const uploadFile = await cloudinary.uploader.upload(fileStr, {
       upload_preset: 'dev_dienlanh'
     })
-    
-      if (req.body._id !== undefined) {
-        Info.updateOne({ _id: _id }, [
-          {
-            $set: {
-              'name': name,
-              'email': email,
-              'address': address,
-              'phone': phone,
-              'facebook': facebook,
-              'zalo': zalo,
-              'tiktok': tiktok,
-              'logo': uploadFile.url,
-              'gps': gps,
-              'introduce': introduce,
-              'paypolicy': paypolicy,
-              'shippolicy': shippolicy,
-              'warrantypolicy': warrantypolicy
-            }
-          }]
-        )
-          .then(re => {
-            res.status(200).json({ mess: 'Thành công', status: true })
-          })
-          .catch(er => {
-            res.status(400).json({ mess: 'Thất bại', status: false })
-          })
+
+    if (req.body._id !== undefined) {
+      Info.updateOne({ _id: _id }, [
+        {
+          $set: {
+            'name': name,
+            'email': email,
+            'address': address,
+            'phone': phone,
+            'facebook': facebook,
+            'zalo': zalo,
+            'tiktok': tiktok,
+            'youtube': youtube,
+            'logo': uploadFile.url,
+            // 'gps': gps,
+            'introduce': introduce,
+            'paypolicy': paypolicy,
+            'shippolicy': shippolicy,
+            'warrantypolicy': warrantypolicy
+          }
+        }]
+      )
+        .then(re => {
+          res.status(200).json({ mess: 'Thành công', status: true })
+        })
+        .catch(er => {
+          res.status(400).json({ mess: 'Thất bại', status: false })
+        })
+    }
+    else {
+      var inf = {
+        name: name,
+        email: email,
+        address: address,
+        phone: phone,
+        facebook: facebook,
+        zalo: zalo,
+        tiktok: tiktok,
+        youtube: youtube,
+        logo: uploadFile.url,
+        // gps: gps,
+        introduce: introduce,
+        paypolicy: paypolicy,
+        shippolicy: shippolicy,
+        warrantypolicy: warrantypolicy
       }
-      else {
-        var inf = {
-          name: name,
-          email: email,
-          address: address,
-          phone: phone,
-          facebook: facebook,
-          zalo: zalo,
-          tiktok: tiktok,
-          logo: uploadFile.url,
-          gps: gps,
-          introduce: introduce,
-          paypolicy: paypolicy,
-          shippolicy: shippolicy,
-          warrantypolicy: warrantypolicy
-        }
-        Info.create(inf)
-          .then(re => {
-            res.status(200).json({ mess: 'Thành công', status: true })
-          })
-          .catch(er => {
-            res.status(400).json({ mess: 'Thất bại',status:false })
-          })
-      }
+      Info.create(inf)
+        .then(re => {
+          res.status(200).json({ mess: 'Thành công', status: true })
+        })
+        .catch(er => {
+          res.status(400).json({ mess: 'Thất bại', status: false })
+        })
+    }
   } catch (error) {
     console.error(error);
   }
