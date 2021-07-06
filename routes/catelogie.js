3d7ba9e7fd6abe24c5d41c62b6b421fde017995c
Minh Tài <tranminhtai365@gmail.com>
2021-07-07 01:08:41 +0700

    cate admin count product

--- routes/catelogie.js
+++ routes/catelogie.js
@@ -1,43 +1,77 @@
 var express = require('express');
 var router = express.Router();
 var Catelogies = require('../models/catelogie');
-
+const Product = require('../models/product');
 //get catelogy
 router.get('/', (req, res, next) => {
-    Catelogies.find((err, dt) => {
-      res.send(dt)
+  // Catelogies.find((err, dt) => {
+  //   res.send(dt)
+  // })
+  Product.aggregate([
+    {
+      $group: {
+        _id: '$catelogyid',
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $lookup: {
+        from: "catelogies",
+        localField: "_id",
+        foreignField: "_id",
+        as: "cate"
+      },
+    },
+    {
+      $project: {
+        "name": "$cate.name",
+        "createdlc": "$cate.createdlc",
+        //  {
+        //   $slice: ["$data", 0, 1],
+        // }
+        count: "$count"
+      }
+    },
+    {
+      $unwind: "$name"
+    },
+    {
+      $unwind: "$createdlc"
+    }
+  ]).then(re => {
+    res.status(200).send(re);
+  })
+})
+//create catelogy
+router.post('/set', (req, res, next) => {
+  if (req.body._id !== undefined) {
+    Catelogies.updateOne({ _id: req.body._id }, [{
+      $set: {
+        "name": req.body.name
+      }
+    }])
+      .then(re => {
+        res.status(200).json({ mess: 'Thành công', status: true })
+      })
+      .catch(er => {
+        res.status(400).json({ mess: 'Thất bại', status: false })
+      })
+  }
+  else {
+    var now = new Date;
+    var nowlc = new Date().toLocaleString();
+    Catelogies.create({
+      name: req.body.name,
+      created: now,
+      createdlc: nowlc
     })
-  })
-  //create catelogy
-  router.post('/set', (req, res, next) => {
-    if (req.body._id !== undefined) {
-      Catelogies.updateOne({ _id: req.body._id }, [{
-        $set: {
-          "name": req.body.name
-        }
-      }])
-      .then(re=>{        
-        res.status(200).json({ mess: 'Thành công',status:true })
+      .then(re => {
+        res.status(200).json({ mess: 'Thành công', status: true })
       })
-      .catch(er=>{
-        res.status(400).json({ mess: 'Thất bại',status:false })
+      .catch(er => {
+        res.status(400).json({ mess: 'Thất bại', status: false })
       })
-    }
-    else {
-      var now = new Date;
-      var nowlc = new Date().toLocaleString();
-      Catelogies.create({
-        name: req.body.name,
-        created: now,
-        createdlc: nowlc
-      })
-      .then(re=>{       
-        res.status(200).json({ mess: 'Thành công',status:true })
-      })
-      .catch(er=>{
-        res.status(400).json({ mess: 'Thất bại',status:false })
-      })
-    }
-  })
-  
+  }
+})
+
 module.exports = router;
