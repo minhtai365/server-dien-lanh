2f9d10663b07b738fefea08579648f1f08488bbe
Minh Tài <tranminhtai365@gmail.com>
2021-07-27 10:40:00 +0700

    test build

--- app.js
+++ app.js
@@ -49,6 +49,12 @@ app.use('/products', productRouter);
 app.use('/slide', slideRouter);
 app.use('/promotion', promotionRouter);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
